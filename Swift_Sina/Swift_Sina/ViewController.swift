105acf76073c72a99b7ca2f2ba2eb2f6ea3b00ea
susu <28025920@qq.com>
2016-08-07 10:54:22 +0800

    初始化项目
    初始化项目

---
+++ Swift_Sina/Swift_Sina/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Sina
+//
+//  Created by  江苏 on 16/8/7.
+//  Copyright © 2016年 Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
