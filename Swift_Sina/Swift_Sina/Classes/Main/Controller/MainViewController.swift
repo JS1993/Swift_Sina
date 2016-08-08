e7c5191f134039a7bee9b9c57dc542554ab7c060
susu <28025920@qq.com>
2016-08-08 10:38:25 +0800

    给发表按钮添加点击事件

--- Swift_Sina/Swift_Sina/Classes/Main/Controller/MainViewController.swift
+++ Swift_Sina/Swift_Sina/Classes/Main/Controller/MainViewController.swift
@@ -30,7 +30,18 @@ extension MainViewController{
         publishBtn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: .Highlighted)
         publishBtn.sizeToFit()
         publishBtn.center=CGPointMake(tabBar.center.x, tabBar.bounds.size.height*0.5)
+        publishBtn.addTarget(self, action:#selector(MainViewController.publishAction), forControlEvents: .TouchUpInside)
         tabBar.addSubview(publishBtn)
+    }
+}
+
+// MARK: - 监听发布按钮点击
+//发送消息是OC特性，将方法@SEL-->类中查找方法-->根据@SEL找到imp指针（函数指针）-->执行函数
+//如果swift中声明一个方法是private，那么该函数不会被添加到方法列表中
+//如果在private加上@objc,那么该方法依然会被添加到方法列表当中
+extension MainViewController{
+    @objc private func publishAction() {
+        print("点击发布按钮")
     }
 }
 
